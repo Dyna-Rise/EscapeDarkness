247e6ca0e43e92a685250a4ea8ff20c6b90e247d
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-09-17 13:30:31 +0900

    Player作成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("プレイヤーの基礎ステータス")]
+    public float playerSpeed = 3.0f;
+
+    float axisH; //横方向の入力状況
+    float axisV; //縦方向の入力状況
+
+    [Header("プレイヤーの角度計算用")]
+    public float angleZ = -90f; //プレイヤーの角度計算用
+
+    [Header("オン/オフの対象スポットライト")]
+    public GameObject spotLight; //対象のスポットライト
+
+    bool inDamage; //ダメージ中かどうかのフラグ管理
+
+    //コンポーネント
+    Rigidbody2D rbody;
+    Animator anime;
+
+    void Start()
+    {
+        //コンポーネントの取得
+        rbody = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+
+        //スポットライトを所持していればスポットライト表示
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
