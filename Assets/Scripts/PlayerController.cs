c724eb2fca443b4187ed2baa1f07ae02c3910dd6
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-09-17 16:02:21 +0900

    Playerアニメ設置

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,6 +36,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move(); //上下左右の入力値の取得
+        angleZ = GetAngle(); //その時の角度を変数angleZに反映
+        Animation(); //angleZを利用してアニメーション
     }
 
     private void FixedUpdate()
@@ -80,7 +82,44 @@ public class PlayerController : MonoBehaviour
         {
             angle = angleZ;
         }
-
             return angle;
+    }
+
+
+    void Animation()
+    {
+        //なんらかの入力がある場合
+        if (axisH != 0 || axisV != 0)
+        {
+
+            //ひとまずRunアニメを走らせる
+            anime.SetBool("run", true);
+
+            //angleZを利用して方角を決める　パラメータdirection int型
+            //int型のdirection 下：0　上：1　右：2　左：それ以外
+
+            if (angleZ > -135f && angleZ < -45f) //下方向
+            {
+                anime.SetInteger("direction", 0);
+            }
+            else if (angleZ >= -45f && angleZ <= 45f) //右方向
+            {
+                anime.SetInteger("direction", 2);
+                transform.localScale = new Vector2(1, 1);
+            }
+            else if (angleZ > 45f && angleZ < 135f) //上方向
+            {
+                anime.SetInteger("direction", 1);
+            }
+            else //左方向
+            {
+                anime.SetInteger("direction", 3);
+                transform.localScale = new Vector2(-1, 1);
+            }
+        }
+        else //何も入力がない場合
+        {
+            anime.SetBool("run", false); //走るフラグをOFF
+        }
     }
 }
